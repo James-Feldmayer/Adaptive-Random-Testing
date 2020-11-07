e5fd7584eda64334fb2088e6de1e13cb5df49b72
James Feldmayer <36408535+DrNoobopolis@users.noreply.github.com>
2020-11-07 15:30:47 +1100

    Add files via upload

--- current.py
+++ current.py
@@ -1,11 +1,11 @@
 
-
 import tkinter as tk
 from threading import Thread
 import re
 import traceback
 import ast
 
+# turn old.py into a library
 
 def file_string(file_location):
     accumulate = ""
@@ -65,10 +65,6 @@ class user_function:
 
             return False
 
-# Get rid of the example conversion label and input box
-# Should just be the output label
-
-# Sync the random inputs up
 
 # Figure out something to test
 
@@ -77,50 +73,65 @@ class user_function:
 
 
 def start_function():
-    if(update_function_under_test()):
-        if(input_domain(IDELabel)):
-            if(update_conversion_function()):
-                print("start")  # all the inputs are correct now can start
+    if(update_gui()):
+        print("start")
 
 
 def stop_function():
+    # if running 
+    # stop running 
+
     print("stop")
 
 
-def update_function_under_test():
+def clear():
     PUTELabel['text'] = ""
     FUTELabel['text'] = ""
     EIELabel['text'] = ""
+    CPELabel['text'] = ""
+    CFELabel['text'] = ""
+    ECLabel['text'] = ""
+    IDELabel['text'] = ""
+    TCLabel['text'] = ""
 
-    function_under_test = user_function(
-        PUTEntry.get(), FUTEntry.get())  # input, input
 
-    if(function_under_test.read(PUTELabel)):  # label
-        if(function_under_test.compile(FUTELabel)):  # label
-            # input, label
+function_under_test = None
+user_conversion_function = None
+
+def update_function_under_test():
+    global function_under_test
+    function_under_test = user_function(PUTEntry.get(), FUTEntry.get())
+
+    if(function_under_test.read(PUTELabel)):
+        if(function_under_test.compile(FUTELabel)):
             return function_under_test.test(EIEntry.get(), EIELabel)
 
     return False
 
 
 def update_conversion_function():
-    CPELabel['text'] = ""
-    CFELabel['text'] = ""
-    ECELabel['text'] = ""
-
+    global user_conversion_function 
     user_conversion_function = user_function(CPEntry.get(), CFEntry.get())
 
     if(user_conversion_function.read(CPELabel)):
         if(user_conversion_function.compile(CFELabel)):
-            return user_conversion_function.test(IDELabel['text'], ECELabel)
+            if(user_conversion_function.test(IDELabel['text'], ECLabel)):
+            
+                # above test ensures this is safe
+                conversion_output = user_conversion_function.function(eval(IDELabel['text']))
+
+                global function_under_test 
+                function_under_test.test(f'"{conversion_output}"', TCLabel)
+
+                # if function_under_test.test() fails it should not prevent the
+                # automated test process from starting 
+                
+                return True
 
     return False
 
 # start calls 
 # update then tries to start timer
-
-# get it working then touch up last few usability things
-
 
 def random_case(input_domain):
     import random
@@ -135,48 +146,61 @@ def random_case(input_domain):
     return output_case
 
 
-def input_domain(label):
+def update_input_domain():
     try:
         input_domain = eval(IDEntry.get())
 
-        label['fg'] = 'lime'
-        label['text'] = f"{random_case(input_domain)}"
+        IDELabel['fg'] = 'lime'
+        IDELabel['text'] = f"{random_case(input_domain)}"
 
         return True
 
     except:
-        label['fg'] = 'red'
-        label['text'] = "Expected an integer list"
+        IDELabel['fg'] = 'red'
+        IDELabel['text'] = "Expected an integer list"
 
         return False
 
+# move input domain down the bottom
+# add label
+# remove globals after adding testing
 
-def update(event):
+def update_gui():
+    clear()
+
     if(update_function_under_test()):
-        if(input_domain(IDELabel)):
-            update_conversion_function()
+        if(update_input_domain()):
+            return update_conversion_function()
+
+    return False
+
+
+def update_event(event):
+    update_gui()
 
 
 ########################################################
 
 
 # Window
-window = tk.Tk()
-window.title("CSCI318: Practical ART")
-window.geometry("850x600")
-window.configure(bg='white')
+root = tk.Tk()
+root.title("CSCI318: Practical ART")
+# root.update_idletasks()
+root.geometry("800x500") # "500x500+50+50"
+root.configure(bg='white')
 
+# I need to resize many of these elements
 
 # PUT = Program Under Test
-PUTLabel = tk.Label(window, bg='white',
+PUTLabel = tk.Label(root, bg='white',
                     font=("Helvetica", 14),
                     text="Program under test:")
 
-PUTEntry = tk.Entry(window, font=("Helvetica", 14),
+PUTEntry = tk.Entry(root, font=("Helvetica", 14),
                     state="normal")
 
-PUTELabel = tk.Label(window, bg='white',
-                     font=("Helvetica", 14))  # , justify=tk.LEFT
+PUTELabel = tk.Label(root, bg='white',
+                     font=("Helvetica", 14))
 
 PUTLabel.grid(row=0, column=0, padx=15, pady=15)
 PUTEntry.grid(row=0, column=1, padx=15, pady=15)
@@ -184,14 +208,14 @@ PUTELabel.grid(row=0, column=2, padx=15, pady=15, sticky="w")
 
 
 # FUT = Function Under Test
-FUTLabel = tk.Label(window, bg='white',
+FUTLabel = tk.Label(root, bg='white',
                     font=("Helvetica", 14),
                     text="Function under test:")
 
-FUTEntry = tk.Entry(window, font=("Helvetica", 14),
+FUTEntry = tk.Entry(root, font=("Helvetica", 14),
                     state="normal")
 
-FUTELabel = tk.Label(window, bg='white',
+FUTELabel = tk.Label(root, bg='white',
                      font=("Helvetica", 14))
 
 FUTLabel.grid(row=1, column=0, padx=15, pady=15)
@@ -200,14 +224,14 @@ FUTELabel.grid(row=1, column=2, padx=15, pady=15, sticky="w")
 
 
 # EI = Example Input
-EILabel = tk.Label(window, bg='white',
+EILabel = tk.Label(root, bg='white',
                    font=("Helvetica", 14),
                    text="Example input:")
 
-EIEntry = tk.Entry(window, font=("Helvetica", 14),
+EIEntry = tk.Entry(root, font=("Helvetica", 14),
                    state="normal")
 
-EIELabel = tk.Label(window, bg='white',
+EIELabel = tk.Label(root, bg='white',
                     font=("Helvetica", 14))
 
 EILabel.grid(row=2, column=0, padx=15, pady=15)
@@ -216,14 +240,14 @@ EIELabel.grid(row=2, column=2, padx=15, pady=15, sticky="w")
 
 
 # ID = Input Domain
-IDLabel = tk.Label(window, bg='white',
+IDLabel = tk.Label(root, bg='white',
                    font=("Helvetica", 14),
                    text="Input domain:")
 
-IDEntry = tk.Entry(window, font=("Helvetica", 14),
+IDEntry = tk.Entry(root, font=("Helvetica", 14),
                    state="normal")
 
-IDELabel = tk.Label(window, bg='white',
+IDELabel = tk.Label(root, bg='white',
                     font=("Helvetica", 14))
 
 IDLabel.grid(row=3, column=0, padx=15, pady=15)
@@ -232,14 +256,14 @@ IDELabel.grid(row=3, column=2, padx=15, pady=15, sticky="w")
 
 
 # CP = Conversion Program
-CPLabel = tk.Label(window, bg='white',
+CPLabel = tk.Label(root, bg='white',
                    font=("Helvetica", 14),
                    text="Conversion program:")
 
-CPEntry = tk.Entry(window, font=("Helvetica", 14),
+CPEntry = tk.Entry(root, font=("Helvetica", 14),
                    state="normal")
 
-CPELabel = tk.Label(window, bg='white',
+CPELabel = tk.Label(root, bg='white',
                     font=("Helvetica", 14))
 
 CPLabel.grid(row=4, column=0, padx=15, pady=15)
@@ -248,14 +272,14 @@ CPELabel.grid(row=4, column=2, padx=15, pady=15, sticky="w")
 
 
 # CF = Conversion Function
-CFLabel = tk.Label(window, bg='white',
+CFLabel = tk.Label(root, bg='white',
                    font=("Helvetica", 14),
                    text="Conversion function:")
 
-CFEntry = tk.Entry(window, font=("Helvetica", 14),
+CFEntry = tk.Entry(root, font=("Helvetica", 14),
                    state="normal")
 
-CFELabel = tk.Label(window, bg='white',
+CFELabel = tk.Label(root, bg='white',
                     font=("Helvetica", 14))
 
 CFLabel.grid(row=5, column=0, padx=15, pady=15)
@@ -264,34 +288,49 @@ CFELabel.grid(row=5, column=2, padx=15, pady=15, sticky="w")
 
 
 # EC = Example conversion
-ECELabel = tk.Label(window, bg='white',
+ECLabel = tk.Label(root, bg='white',
                     font=("Helvetica", 14))
 
-ECELabel.grid(row=6, column=0, padx=15, pady=15, columnspan=3, sticky="w")
+ECLabel.grid(row=6, column=0, padx=15, pady=15, columnspan=3, sticky="w")
+
+# TC = Test conversion
+TCLabel = tk.Label(root, bg='white',
+                    font=("Helvetica", 14))
+
+TCLabel.grid(row=7, column=0, padx=15, pady=15, columnspan=3, sticky="w")
+
 
 
 # start button
-StartButton = tk.Button(window, font=("Helvetica", 14),
+StartButton = tk.Button(root, font=("Helvetica", 14),
                         text="Start",
                         command=start_function)
 
-StartButton.grid(row=7, column=0, padx=15, pady=15)
+StartButton.grid(row=8, column=0, padx=15, pady=15)
 
 
 # stop button
-StopButton = tk.Button(window, font=("Helvetica", 14),
+StopButton = tk.Button(root, font=("Helvetica", 14),
                        text="Stop",
                        command=stop_function)
 
-StopButton.grid(row=7, column=1, padx=15, pady=15)
+StopButton.grid(row=8, column=1, padx=15, pady=15)
 
 
-PUTEntry.bind("<Any-KeyRelease>", update)  # bind to any keyrelease
-FUTEntry.bind("<Any-KeyRelease>", update)
-EIEntry.bind("<Any-KeyRelease>", update)
-IDEntry.bind("<Any-KeyRelease>", update)
-CPEntry.bind("<Any-KeyRelease>", update)
-CFEntry.bind("<Any-KeyRelease>", update)
+# start button
+BottomButton = tk.Button(root, font=("Helvetica", 14),
+                        text="Start",
+                        command=start_function)
+
+BottomButton.grid(row=9, column=0, padx=15, pady=15)
+
+
+PUTEntry.bind("<Any-KeyRelease>", update_event)  # bind to any keyrelease
+FUTEntry.bind("<Any-KeyRelease>", update_event)
+EIEntry.bind("<Any-KeyRelease>", update_event)
+IDEntry.bind("<Any-KeyRelease>", update_event)
+CPEntry.bind("<Any-KeyRelease>", update_event)
+CFEntry.bind("<Any-KeyRelease>", update_event)
 
 # Start GUI
-window.mainloop()
+root.mainloop()
